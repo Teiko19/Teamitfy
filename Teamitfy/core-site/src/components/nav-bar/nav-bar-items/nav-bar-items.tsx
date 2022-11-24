ea0c9049bb8c39d5911fa701d84074d4fdbbc75a
Teiko19 <tomi.belvedere@gmail.com>
2022-11-23 22:43:17 -0300

    v1.1

--- Teamitfy/core-site/src/components/nav-bar/nav-bar-items/nav-bar-items.tsx
+++ Teamitfy/core-site/src/components/nav-bar/nav-bar-items/nav-bar-items.tsx
@@ -2,10 +2,14 @@ import { Nav } from "react-bootstrap";
 
 import styles from './nav-bar-items.module.scss';
 
-export const NavBarItems = () => (
-  <>
-    <Nav.Link className={styles.item} href="#action1">Services</Nav.Link>
-    <Nav.Link className={styles.item} href="#action2">Portfolio</Nav.Link>
-    <Nav.Link className={styles.item} href="#action3">Contact</Nav.Link>
+interface Info {
+  act: string,
+  type: string,
+  id: string
+}
+
+export const NavBarItems = ({items}: any) => {
+  return <>
+    {items.map(({ act , type , id }: Info) => (<Nav.Link key={`key-${id}`} className={styles.item} href={act}> {type} </Nav.Link>))}
   </>
-)
+};
--- Teamitfy/core-site/src/components/nav-bar/nav-bar.tsx
+++ Teamitfy/core-site/src/components/nav-bar/nav-bar.tsx
@@ -3,28 +3,32 @@ import { Navbar, Container, Offcanvas, Nav } from "react-bootstrap";
 import { NavBarItems } from "./nav-bar-items/nav-bar-items";
 import styles from './nav-bar.module.scss'
 
-export const NavBar = () => (
-  <Navbar expand={false} className={styles.navbar}>
-    <Container fluid>
-      <Navbar.Brand className='lead' href="#">Teamitfy</Navbar.Brand>
-      <Nav className="d-flex justify-content-end flex-row d-none d-lg-flex">
-        <NavBarItems />
+export const NavBar = () => {
+  const items =  [
+    { act: "#action1" , type: "Sevices" , id: "1" } ,
+    { act: "#action2" , type: "Portafolio" , id: "2" } ,
+    { act: "#action3" , type: "Contact" , id: "3" }
+  ];
+  return <Navbar expand={false} className={styles.navbar}>
+  <Container fluid>
+    <Navbar.Brand className='lead' href="#">Teamitfy</Navbar.Brand>
+    <Nav className="d-flex justify-content-end flex-row d-none d-lg-flex">
+      <NavBarItems items={items} />
+    </Nav>
+    <Navbar.Toggle aria-controls="offcanvasNavbar" className="d-lg-none"/>
+    <Navbar.Offcanvas
+      id="offcanvasNavbar"
+      aria-labelledby="offcanvasNavbarLabel"
+      placement="end">
+    <Offcanvas.Header closeButton>
+      <Offcanvas.Title id="offcanvasNavbarLabel">Teamitfy</Offcanvas.Title>
+    </Offcanvas.Header>
+    <Offcanvas.Body>
+      <Nav className="justify-content-end flex-grow-1 pe-3">
+        <NavBarItems items={items} />
       </Nav>
-      <Navbar.Toggle aria-controls="offcanvasNavbar" className="d-lg-none"/>
-      <Navbar.Offcanvas
-        id="offcanvasNavbar"
-        aria-labelledby="offcanvasNavbarLabel"
-        placement="end"
-      >
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title id="offcanvasNavbarLabel">Teamitfy</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <Nav className="justify-content-end flex-grow-1 pe-3">
-            <NavBarItems />
-          </Nav>
-        </Offcanvas.Body>
-      </Navbar.Offcanvas>
-    </Container>
-  </Navbar>
-);
+    </Offcanvas.Body>
+    </Navbar.Offcanvas>
+  </Container>
+</Navbar>
+}
