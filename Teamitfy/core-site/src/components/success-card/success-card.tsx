832f836a15daccd133652b033bb413ec567aa721
Teiko19 <tomi.belvedere@gmail.com>
2022-11-24 18:22:43 -0300

    test1

--- Teamitfy/core-site/src/components/success-card/success-card.tsx
+++ Teamitfy/core-site/src/components/success-card/success-card.tsx
@@ -13,9 +13,9 @@ interface SuccesCardItems {
 
 export const SuccessCard = () => {
   const items: SuccesCardItems[] = [
-    { typeClass: `${styles.border_card}`, typeClassImg: `${styles.happy}`, nameLink: "/images/launching.png", name: "Projects Launched"},
-    { typeClass: `${styles.border_card}`, typeClassImg: `${styles.happy}`, nameLink: "/images/happy-face.png", name: "Happy customers"},
-    { typeClass: "", typeClassImg: `${styles.target}`, nameLink: "/images/target.png", name: "Applications planned"}
+    { typeClass: styles.border_card, typeClassImg: styles.launching, nameLink: "/images/launching.png", name: "Projects Launched"},
+    { typeClass: styles.border_card, typeClassImg: styles.happy, nameLink: "/images/happy-face.png", name: "Happy customers"},
+    { typeClass: "", typeClassImg: styles.target, nameLink: "/images/target.png", name: "Applications planned"}
   ];
   return (
       <Container>
@@ -23,10 +23,10 @@ export const SuccessCard = () => {
       <Row className={`border ${styles.container}`}>
         {items.map(({ typeClass , typeClassImg , nameLink , name }) => (
             <>
-              <Col xs={12} md={4} className={`mb-4 ${styles.image_container}` + {typeClass}}>
+              <Col xs={12} md={4} className={`mb-4 ${styles.image_container} ${typeClass}`}>
                 <div className={`d-flex align-items-center justify-content-center flex-column`}>
                   <img
-                    className={`img-fluid ${styles.images}` + {typeClassImg}}
+                    className={`img-fluid ${styles.images} ${typeClassImg}`}
                     src={nameLink}
                     alt="launching"
                   />
