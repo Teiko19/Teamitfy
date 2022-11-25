f6051735241676fb75d9af6403fe0fa5761458a0
Teiko19 <tomi.belvedere@gmail.com>
2022-11-25 20:01:28 -0300

    v1.2

--- Teamitfy/core-site/src/components/success-card/success-card.tsx
+++ Teamitfy/core-site/src/components/success-card/success-card.tsx
@@ -40,39 +40,3 @@ export const SuccessCard = () => {
     </Container>
   );
 };
-
-/*    
-      <Col xs={12} md={4} className={`mb-4 ${styles.image_container} ${styles.border_card}`}>
-        <div className={`d-flex align-items-center justify-content-center flex-column`}>
-          <img
-            className={`img-fluid ${styles.images} ${styles.launching}`}
-            src="/images/launching.png"
-            alt="launching"
-            />
-          <h3>100+</h3>
-          <Card.Text>Projects Launched</Card.Text>
-        </div>
-      </Col>
-      <Col xs={12} md={4} className={`mb-4 ${styles.image_container} ${styles.border_card}`}>
-        <div className={`d-flex align-items-center justify-content-center flex-column`}>
-          <img
-            className={`img-fluid ${styles.images} ${styles.happy}`}
-            src="/images/happy-face.png"
-            alt="launching"
-            />
-          <h3>100+</h3>
-          <Card.Text>Happy customers</Card.Text>
-        </div>
-      </Col>
-      <Col xs={12} md={4} className={`mb-4 ${styles.image_container}`}>
-        <div className={`d-flex align-items-center justify-content-center flex-column`}>
-          <img
-            className={`img-fluid ${styles.images} ${styles.target}`}
-            src="/images/target.png"
-            alt="launching"
-            />
-          <h3>100+</h3>
-          <Card.Text>Applications planned</Card.Text>
-        </div>
-      </Col> 
-*/
--- Teamitfy/core-site/src/components/what-we-do-card/what-we-do-card.tsx
+++ Teamitfy/core-site/src/components/what-we-do-card/what-we-do-card.tsx
@@ -3,11 +3,50 @@ import { Container, Title } from "../../ui";
 import { HeartbeatSvg, ServerSvg, WebDesignSvg, WebDevelopmentSvg, SandWatchSvg, NextSvg } from "../../svg";
 
 import styles from './what-we-do.module.scss';
+import { arrayBuffer } from "stream/consumers";
 
-export const WhatWeDo = () => (
-  <Container>
-    <Title titleText="What we do!" className={styles.title}/>
-    <Row className={styles.row}>
+export const WhatWeDo = () => {
+
+  const arrMax = [
+    {rowProp: styles.row , 
+      arr:  [
+        {imageStyles: ``, propsComp: <WebDesignSvg fillColor="white"/>},
+        {imageStyles: `${styles.image_container_middle}`, propsComp: <WebDevelopmentSvg color="white"/>},
+        {imageStyles: ``, propsComp: <WebDevelopmentSvg />},
+      ]},
+    {rowProp: "",
+      arr:  [
+        {imageStyles: ``, propsComp: <HeartbeatSvg fillColor="white"/>},
+        {imageStyles: `${styles.image_container_middle}`, propsComp: <ServerSvg color="white"/>},
+        {imageStyles: ``, propsComp: <SandWatchSvg />}
+      ]}
+  ];
+
+  return (
+    <Container> 
+      <Title titleText="What we do!" className={styles.title}/>
+      {arrMax.map( ({rowProp, arr}) => (
+        <Row className={rowProp}>
+          {arr.map(({imageStyles , propsComp}) => (
+            <Col xs={12} md={4} className={styles.column}>
+              <div className={`bg-white d-flex flex-column align-items-center ${styles.card}`}>
+                <div className={`${styles.image_container} ${imageStyles}`}>
+                  {propsComp}
+                </div>
+                <h4>Website design</h4>
+                <p>We are partnered with the best design studios, configured for an effortless collaboration among teams</p>
+                <NextSvg />
+              </div>
+            </Col>
+          ))}
+        </Row>
+      ))}
+    </Container>
+  )
+  
+};
+
+{/*    <Row className={styles.row}>
         <Col xs={12} md={4} className={styles.column}>
           <div className={`bg-white d-flex flex-column align-items-center ${styles.card}`}>
             <div className={styles.image_container}>
@@ -70,6 +109,4 @@ export const WhatWeDo = () => (
             <NextSvg />
           </div>
         </Col>
-      </Row>
-  </Container>
-);
+      </Row> */}
