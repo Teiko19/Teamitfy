d3d638bf7616e2df13beab76c363c8f1f4a6b6ae
Teiko19 <tomi.belvedere@gmail.com>
2022-12-19 20:23:53 -0300

    finish

--- Teamitfy/core-site/src/components/what-we-do-card/what-we-do-card.tsx
+++ Teamitfy/core-site/src/components/what-we-do-card/what-we-do-card.tsx
@@ -1,12 +1,10 @@
 import { Row, Col} from "react-bootstrap";
 import { Container, Title } from "../../ui";
 import { HeartbeatSvg, ServerSvg, WebDesignSvg, WebDevelopmentSvg, SandWatchSvg, NextSvg } from "../../svg";
-
 import styles from './what-we-do.module.scss';
 import { arrayBuffer } from "stream/consumers";
 
 export const WhatWeDo = () => {
-
   const arrMax = [
     {rowProp: styles.row , 
       arr:  [
@@ -21,7 +19,6 @@ export const WhatWeDo = () => {
         {imageStyles: ``, propsComp: <SandWatchSvg />}
       ]}
   ];
-
   return (
     <Container> 
       <Title titleText="What we do!" className={styles.title}/>
@@ -43,70 +40,4 @@ export const WhatWeDo = () => {
       ))}
     </Container>
   )
-  
 };
-
-{/*    <Row className={styles.row}>
-        <Col xs={12} md={4} className={styles.column}>
-          <div className={`bg-white d-flex flex-column align-items-center ${styles.card}`}>
-            <div className={styles.image_container}>
-              <WebDesignSvg fillColor="white"/>
-            </div>
-            <h4>Website design</h4>
-            <p>We are partnered with the best design studios, configured for an effortless collaboration among teams</p>
-            <NextSvg />
-          </div>
-        </Col>
-        <Col xs={12} md={4} className={styles.column}>
-          <div className={`bg-white d-flex flex-column align-items-center ${styles.card}`}>
-            <div className={`${styles.image_container} ${styles.image_container_middle}`}>
-              <WebDevelopmentSvg color="white"/>
-            </div>
-            <h4>Website design</h4>
-            <p>We are partnered with the best design studios, configured for an effortless collaboration among teams</p>
-            <NextSvg />
-          </div>
-        </Col>
-        <Col xs={12} md={4} className={styles.column}>
-          <div className={`bg-white d-flex flex-column align-items-center ${styles.card}`}>
-            <div className={styles.image_container}>
-              <WebDevelopmentSvg />
-            </div>
-            <h4>Website design</h4>
-            <p>We are partnered with the best design studios, configured for an effortless collaboration among teams</p>
-            <NextSvg />
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={12} md={4} className={styles.column}>
-          <div className={`bg-white d-flex flex-column align-items-center ${styles.card}`}>
-            <div className={styles.image_container}>
-              <HeartbeatSvg fillColor="white"/>
-            </div>
-            <h4>Website design</h4>
-            <p>We are partnered with the best design studios, configured for an effortless collaboration among teams</p>
-            <NextSvg />
-          </div>
-        </Col>
-        <Col xs={12} md={4} className={styles.column}>
-          <div className={`bg-white d-flex flex-column align-items-center ${styles.card}`}>
-            <div className={`${styles.image_container} ${styles.image_container_middle}`}>
-              <ServerSvg color="white"/>
-            </div>
-            <h4>Website design</h4>
-            <p>We are partnered with the best design studios, configured for an effortless collaboration among teams</p>
-            <NextSvg />
-          </div>
-        </Col>
-        <Col xs={12} md={4} className={styles.column}>
-          <div className={`bg-white d-flex flex-column align-items-center ${styles.card}`}>
-            <div className={styles.image_container}>
-              <SandWatchSvg />
-            </div>
-            <h4>Website design</h4>
-            <p>We are partnered with the best design studios, configured for an effortless collaboration among teams</p>
-            <NextSvg />
-          </div>
-        </Col>
-      </Row> */}
